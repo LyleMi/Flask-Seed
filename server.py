f2ec453def4848cc8cd27aae6c60864f51771ffe
LyleMi <1278919492@qq.com>
2017-05-28 08:25:30 +0800

    add-some-function

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, flash
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import session, flash
+
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
 
@@ -6,6 +14,18 @@ app.secret_key = 'my_secret_key'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/get")
+def getAction():
+    param = request.args.get("param", "0")
+    return param
+
+
+@app.route("/post", methods=['POST'])
+def postAction():
+    data = request.form["data"]
+    return data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
